fe8b4bd64b98c7e9b58458cbb8138d5916438bc2
manuel hagleitner <m.hagleitner@students.htl-leonding.ac.at>
2024-10-24 08:49:28 +0200

    commit tictactoe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TicTacToeComponent} from './tictactoe/tictactoe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TicTacToeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
